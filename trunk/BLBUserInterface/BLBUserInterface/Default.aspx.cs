728e0a3cdab0872c3649bffc609391284c60d1c3
kulkarni.niranjan88@gmail.com <kulkarni.niranjan88@gmail.com@ae09c49c-1257-df19-2e46-50f88c16ec1c>
2012-10-10 14:58:19 +0000

    BLB Service and simple front end implemented

---
+++ trunk/BLBUserInterface/BLBUserInterface/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace BLBUserInterface
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private BLBService.BLBService service;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            service = new BLBService.BLBService();
+            PopulateBondsTable();
+        }
+
+        private void PopulateBondsTable()
+        {
+            DataSet ds = service.GetBonds();
+            GVBonds.DataSource = ds.Tables[0];
+            GVBonds.DataBind();
+        }
+    }
+}
