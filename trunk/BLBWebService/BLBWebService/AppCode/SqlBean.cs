0fd8d9c0e0487814f15ed6d1b2c31bd1ff6ae590
kulkarni.niranjan88@gmail.com <kulkarni.niranjan88@gmail.com@ae09c49c-1257-df19-2e46-50f88c16ec1c>
2012-10-10 16:09:44 +0000

    Login back-end functionality

--- trunk/BLBWebService/BLBWebService/AppCode/SqlBean.cs
+++ trunk/BLBWebService/BLBWebService/AppCode/SqlBean.cs
@@ -9,17 +9,38 @@ namespace BLBWebService.AppCode
 {
     public class SqlBean
     {
+        private SqlConnection _con;
+
         public SqlBean()
         {
+            this._con = new SqlConnection("Server=.;Database=BLBData;Integrated Security=SSPI;"); 
         }
 
         public DataSet GetBonds()
         {
             DataSet ds = new DataSet();
-            SqlConnection con = new SqlConnection("Server=.;Database=BLBData;Integrated Security=SSPI;"); 
-            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Bonds ORDER BY MaturityDate", con);
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Bonds ORDER BY MaturityDate", _con);
             da.Fill(ds, "Fills");
             return ds;
+        }
+
+        public bool Authenticate(string username, string password)
+        {
+            string query = "SELECT password FROM Credentials WHERE username=\'" + username + "\'";
+            SqlDataAdapter da = new SqlDataAdapter(query, _con);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            da.Fill(ds, "Fills");
+            dt = ds.Tables["Fills"];
+            if (dt.Rows.Count == 0)
+                return false;
+            foreach (DataRow dr in dt.Rows)
+            {
+                string pwd = dr[0].ToString();
+                if (pwd.Equals(password))
+                    return true;
+            }
+            return false;
         }
     }
 }
