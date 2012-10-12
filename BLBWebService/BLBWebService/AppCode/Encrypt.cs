181645702b18be8d5a28d9d7cea33a5ba0023843
kulkarni.niranjan88@gmail.com <kulkarni.niranjan88@gmail.com@ae09c49c-1257-df19-2e46-50f88c16ec1c>
2012-10-12 15:47:24 +0000

    MD5 hashing of passwords functionality

---
+++ BLBWebService/BLBWebService/AppCode/Encrypt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BLBWebService.AppCode
+{
+    public class Encrypt
+    {
+        public static string encryptPassword(string password)
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(password);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
