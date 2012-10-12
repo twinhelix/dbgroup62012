4d1e920884422f343cf8e23e1d416988845de2ce
kulkarni.niranjan88@gmail.com <kulkarni.niranjan88@gmail.com@ae09c49c-1257-df19-2e46-50f88c16ec1c>
2012-10-12 15:48:58 +0000

    login UI

---
+++ BLBUserInterface/BLBUserInterface/Login.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BLBUserInterface
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        private BLBService.BLBService service = new BLBService.BLBService();
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        protected void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            bool result = service.Login(TextBoxUsername.Text, TextBoxPassword.Text);
+            if (result)
+            {
+                TextBoxUsername.Text = "Hi";
+            }
+        }
+    }
+}
